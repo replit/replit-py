c30a815c5f6e5d7d027233fcdcb6d72054eab72c
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-07-27 15:05:39 -0700

    Bump version in docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2020, repl.it"
 author = "repl.it"
 
 # The full version, including alpha/beta/rc tags
-release = "1.2.0"
+release = "1.3.0"
 
 
 # -- General configuration ---------------------------------------------------
