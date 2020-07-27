3bfde056a0cc612a4a7de57bf89388b202c5baaa
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-07-27 14:51:35 -0700

    Move clear function from audio to main

--- src/replit/__init__.py
+++ src/replit/__init__.py
@@ -2,4 +2,10 @@
 from .audio import Audio
 from .database import db
 
+
+def clear() -> None:
+    "Clear is used to clear the terminal."
+    print("\033[H\033[2J", end="", flush=True)
+
+
 audio = Audio()
