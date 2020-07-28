d535da8977d7481d1f4bba9f39300b788f50e869
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-07-27 17:21:50 -0700

    Import database relatively

--- src/replit/maqpi/__init__.py
+++ src/replit/maqpi/__init__.py
@@ -8,10 +8,8 @@ from . import html
 from .app import App
 from .html import Page, Paragraph
 from .utils import sign_in_snippet, signin
+from ..database import db
 
-# TODO: import new db package package name
-
-db = ReplitDb(os.environ["REPLIT_DB_URL"])
 auth = LocalProxy(lambda: flask.request.auth)
 signed_in = LocalProxy(lambda: flask.request.signed_in)
 
