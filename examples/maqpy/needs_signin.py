b320857d56a80ba3988f79c6634e709eff321a46
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-07-28 16:26:12 -0700

    Fix typo

--- examples/maqpy/needs_signin.py
+++ examples/maqpy/needs_signin.py
@@ -4,14 +4,14 @@ app = maqpy.App(__name__)
 
 
 @app.route("/")
-@needs_signin(login_html=f"Hello! {maqpy.sign_in_snippet}")
+@maqpy.needs_signin(login_html=f"Hello! {maqpy.sign_in_snippet}")
 def index():
     return "Index function"
 
 
 # needs_signin can also be called with no args
 @app.route("/test")
-@needs_signin
+@maqpy.needs_signin
 def test():
     return "Test function"
 
