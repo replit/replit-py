f175159e97182ed0007f7d5bda82d6fbbaa9642c
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-07-29 11:06:44 -0700

    Import needs_params in maqpy init

--- src/replit/maqpy/__init__.py
+++ src/replit/maqpy/__init__.py
@@ -9,7 +9,7 @@ from . import html
 from .app import App
 from .files import File
 from .html import HTMLElement, Link, Page, Paragraph
-from .utils import needs_signin, sign_in_snippet, signin
+from .utils import needs_params, needs_signin, sign_in_snippet, signin
 from ..database import db
 
 auth = LocalProxy(lambda: flask.request.auth)
