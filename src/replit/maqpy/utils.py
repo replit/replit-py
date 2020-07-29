2ad93fed96ab8e4e9269e228478982032af1897f
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-07-29 11:26:38 -0700

    Add default for needs_params src

--- src/replit/maqpy/utils.py
+++ src/replit/maqpy/utils.py
@@ -56,7 +56,7 @@ def needs_signin(func: Callable = None, login_html: str = sign_in_snippet) -> Ca
 
 def needs_params(
     *param_names: str,
-    src: Union[str, dict],
+    src: Union[str, dict] = "form",
     onerror: Callable[[str], flask.Response] = None,
 ) -> Callable:
     """Require paramaters before a handler can be activated.
