8ce9d9903e74ac70a0e93c82a5181556eed43e22
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-08-04 12:36:23 -0700

    Import new utils functions in maqpy init

--- src/replit/maqpy/__init__.py
+++ src/replit/maqpy/__init__.py
@@ -9,7 +9,15 @@ from . import html
 from .app import App
 from .files import File
 from .html import HTMLElement, Link, Page, Paragraph
-from .utils import needs_params, needs_signin, sign_in_snippet, signin
+from .utils import (
+    needs_params,
+    needs_sign_in,
+    sign_in_snippet,
+    sign_in_page,
+    sign_in,
+    local_redirect,
+    authed_ratelimit,
+)
 from ..database import db
 
 auth = LocalProxy(lambda: flask.request.auth)
