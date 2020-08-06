2f4b9fb48cf05cbe5fa93b2202e7c740c0d11f37
techpixel <68567672+techpixel@users.noreply.github.com>
2020-08-06 00:14:13 -0500

    Add Backwards Compatibility Support

--- src/replit/__init__.py
+++ src/replit/__init__.py
@@ -4,4 +4,8 @@ from . import utils
 from .audio import Audio
 from .database import db
 
+def clear() -> None:
+    """Clear the terminal."""
+    print("\033[H\033[2J", end="", flush=True)
+ 
 audio = Audio()
