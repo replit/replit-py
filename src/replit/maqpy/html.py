c65523f81a2d303c5e7337d1062ad285c166f49f
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-08-13 16:23:24 -0700

    Fix liniting errors in maqpy.html

--- src/replit/maqpy/html.py
+++ src/replit/maqpy/html.py
@@ -1,6 +1,5 @@
 """Python object representations of HTML."""
 from abc import ABC
-from dataclasses import dataclass
 
 import flask
 
@@ -62,12 +61,13 @@ class Page(flask.Response):
 
     def __init__(self, title: str = None, head: str = "", body: str = "") -> None:
         """Initialize the class.
-        
-    Args:
-      title (str): The title of the page. If not provided no title tag will be sent.
-      head (str): The HTML to put in the head of the page. Defaults to nothing.
-      body (str): The HTML to put in the body of the page. Defaults to nothing.
-    """
+
+        Args:
+            title (str): The title of the page. If not provided no title tag will be
+                added.
+            head (str): The HTML to put in the head of the page. Defaults to nothing.
+            body (str): The HTML to put in the body of the page. Defaults to nothing.
+        """
         self.title = title
         self.head = head
         self.body = body
