084e7683385f6d7388b04c7350c88e72e04fd987
IreTheKID <62220201+IreTheKID@users.noreply.github.com>
2020-12-08 13:34:50 -0500

    Added "nuke" and "del" commands

--- src/replit/__main__.py
+++ src/replit/__main__.py
@@ -1,3 +1,4 @@
+import os
 import json
 import click
 
@@ -7,6 +8,10 @@ from replit import db as database
 
 def wrap(color: term.Color, value: str):
     return color.fg + value + term.reset
+
+
+def info(value: str):
+    return term.brightblue.fg + value + term.reset
 
 
 def success(value: str):
@@ -33,11 +38,15 @@ def cli():
 def list_keys(file_path: str):
     """Writes all keys in the DB to a JSON file."""
 
-    file = open(file_path, 'w+')
+    try:
+        file = open(file_path, 'w+')
+    except:
+        click.echo(failure(f"No such file or directory '{file_path}'"))
+    
     keys = list(database.keys())
     json.dump(keys, file)
 
-    click.echo(f"Ouput successfully dumped to '{file_path}'")
+    click.echo(success(f"Ouput successfully dumped to '{file_path}'"))
 
 
 @cli.command(name="match")
@@ -59,14 +68,62 @@ def find_matches(prefix: str):
 @click.argument("val")
 def set_value(key: str, val: str):
     """Sets a DB key to a given value. (Dynamically Typed)"""
-
-    val = eval(val, {})
+        
     try:
+        val = eval(val, {})
         database[key] = val
     except:
         click.echo(failure(f"An error occured while setting DB[{key}] to '{val}'"))
+    else:
+        click.echo(success(f"DB[{key}] was successfully set to '{val}'"))
+        click.echo(info(f"Dynamically typed to {type(val)}"))
 
-    click.echo(success(f"DB[{key}] was successfully set to '{val}'"))
+
+@cli.command(name="del")
+@click.argument("key")
+def del_value(key: str):
+    """Deletes the key-value pair of the given key."""
+
+    try:
+        val = database[key]
+    except KeyError:
+        click.echo(failure(f"The key '{key}' was not found in the DB."))
+    else:
+        click.echo(success(f"The value '{val}' was found at db['{key}']"))
+        flag = str(input("Confirm delete? (y/n): "))
+        click.echo()
+
+        if flag == "y":
+            del database[key]
+            click.echo(success(f"db['{key}'] was successfully deleted."))
+        else:
+            click.echo(info(f"Delete operation cancelled."))
+
+
+@cli.command(name="nuke")
+def nuke_db():
+    """Wipes ALL key-value pairs in the DB."""
+
+    if os.getenv("is_admin", default=False):
+        flag = str(input("Are you sure you want to nuke the DB? (y/n):"))
+
+        if flag == "y":
+            flag = str(input("Ok, but like, REALLY sure? (y/n):"))
+
+            if flag == "y":
+                click.echo(info("Beginning Nuke operation...\n"))
+
+                keys = list(database.keys())
+                for k in keys: del database[k]
+
+                click.echo(success("Nuke operation successful."))
+            else:
+                click.echo(info("Nuke operation cancelled. (close one!)"))
+        else:
+            click.echo(info("Nuke operation cancelled."))        
+    else:
+        click.echo(failure("Only repl owners have access to this command."))
+
 
 
 @cli.command(name="all")
@@ -74,13 +131,17 @@ def set_value(key: str, val: str):
 def list_all(file_path: str):
     """Writes all keys and values in the DB to a JSON file."""
 
-    file = open(file_path, 'w+')
-    keys = list(database.keys())
-    binds = dict([(k, database[k]) for k in keys])
-    
-    json.dump(binds, file)
+    try:
+        file = open(file_path, 'w+')
+    except:
+        click.echo(failure(f"No such file or directory '{file_path}'"))
+    else:
+        keys = list(database.keys())
+        binds = dict([(k, database[k]) for k in keys])
+        
+        json.dump(binds, file)
 
-    click.echo(success(f"Ouput successfully dumped to '{file_path}'"))
+        click.echo(success(f"Output successfully dumped to '{file_path}'"))
 
 
 if __name__ == "__main__":
