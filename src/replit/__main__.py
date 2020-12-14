440d865af6eca199e37fa8d5bdcbfff585a0c23a
IreTheKID <62220201+IreTheKID@users.noreply.github.com>
2020-12-14 13:08:23 -0500

    Final commit

--- src/replit/__main__.py
+++ src/replit/__main__.py
@@ -1,4 +1,4 @@
-"""CLI for interacting with your Repl's DB. Written as top-level script to be run from a shell."""
+"""CLI for interacting with your Repl's DB. Written as top-level script."""
 import json
 
 import click
