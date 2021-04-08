ba91ee590dcd002ba138a01becf212d3d56529f5
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-04-07 18:15:12 -0700

    Setup sphinx documentation for CLI

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx_autodoc_typehints",
+    "sphinx_click",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
