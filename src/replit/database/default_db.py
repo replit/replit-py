5a93f6a1454d49086fd7c84c3b53303e24b80135
brenoafb <37266043+brenoafb@users.noreply.github.com>
2023-05-10 13:54:02 +0000

    Periodically reload DB

--- src/replit/database/default_db.py
+++ src/replit/database/default_db.py
@@ -1,18 +1,34 @@
 """A module containing the default database."""
-from os import environ
+from os import environ, path
 from typing import Optional
+import threading
 
 from .database import Database
 
 db: Optional[Database]
-if os.path.exists("/tmp/replitdb"):
-    with open(file_path, 'r') as file:
-        db_url = file.read()
-else:
-    db_url = environ.get("REPLIT_DB_URL")
 
-if db_url:
-    db = Database(db_url)
-else:
-    # The user will see errors if they try to use the database.
-    db = None
+
+def reload_db() -> None:
+    """
+    Reloads the database. The database token expires every 20h.
+    """
+    global db
+    if path.exists("/tmp/replitdb"):
+        with open("/tmp/replitdb", 'r') as file:
+            db_url = file.read()
+    else:
+        db_url = environ.get("REPLIT_DB_URL")
+
+    if db_url:
+        db = Database(db_url)
+    else:
+        # The user will see errors if they try to use the database.
+        db = None
+
+
+def refresh_db() -> None:
+    """Refresh the DB URL every hour"""
+    threading.Timer(3600, reload_db).start()
+
+
+refresh_db()
